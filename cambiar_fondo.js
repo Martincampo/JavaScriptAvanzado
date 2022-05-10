9d4c650ad7280c9a04ebf5d0f298a642b7fc46fa
Martin Campo <103222585+Martincampo@users.noreply.github.com>
2022-05-10 17:14:15 -0300

    javaScript avanzado

---
+++ cambiar_fondo.js
@@ -0,0 +1,11 @@
+function cambiar(){
+    document.body.style.backgroundColor = 'green';
+}
+
+function color(){
+    document.body.style.backgroundColor = 'red';
+}
+
+function fondo(){
+    document.body.style.backgroundColor = 'blue';
+}
